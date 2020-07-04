3eba1c7db740d6702d8d21059b12fed02182eb67
wribeiroo <wr.ozorio@gmail.com>
2020-07-04 17:58:40 -0300

    Python

--- operadores_arit.py
+++ operadores_arit.py
@@ -18,3 +18,16 @@ print(x//y) # divisão inteira
 print(x%y) # resto da divisão entre dois n. inteiros
 print(x**y) # exponenciação
 
+# operação com strings
+text1 = 'Tom '
+text2 = 'Ribeiro '
+name = text1 + text2
+print('')
+
+print(name)
+print(name * 3)
+
+# métodos
+print(name.upper()) # todas letras maiúsculas
+print(name.capitalize()) # primeira letra maiúscula 
+
