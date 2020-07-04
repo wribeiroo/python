91bd26f849d5787a17b34bf1be54284694dfc1a6
wribeiroo <wr.ozorio@gmail.com>
2020-07-04 17:45:55 -0300

    Python

---
+++ operadores_arit.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# operação com números
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+print('')
+
+# operação com variáveis
+x = 5
+y = 3
+print(x+y)
+
+# outras operações
+
+print(x//y) # divisão inteira
+print(x%y) # resto da divisão entre dois n. inteiros
+print(x**y) # exponenciação
+
---
+++ type.py
@@ -0,0 +1,21 @@
+# checar tipo
+
+type(5) #int
+type('Tom') #str
+type([]) #list
+type({}) #dict
+x = 4/3
+type(x) #float
+type('10') #str
+type(2+3j) #complex
+
+
+# # # # # # # # # # # # # PALAVRAS RESERVADAS # # # # # # # # # # # # # #
+# and  		del			from	  		None			True            #
+# as		elif		global	    	nonlocal		try             #
+# assert    else		if				not			    while           #
+# break		except		import			or	    		with            #
+# class		False		in			    pass			yield           #
+# continue	finaly		is				raise			def             #
+# for	    lambda		return                                          #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
