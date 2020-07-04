24dbaeaa7ecdd98c262770413242aa2130541591
wribeiroo <wr.ozorio@gmail.com>
2020-07-04 17:47:11 -0300

    Python

--- type.py
+++ type.py
@@ -9,13 +9,3 @@ type(x) #float
 type('10') #str
 type(2+3j) #complex
 
-
-# # # # # # # # # # # # # PALAVRAS RESERVADAS # # # # # # # # # # # # # #
-# and  		del			from	  		None			True            #
-# as		elif		global	    	nonlocal		try             #
-# assert    else		if				not			    while           #
-# break		except		import			or	    		with            #
-# class		False		in			    pass			yield           #
-# continue	finaly		is				raise			def             #
-# for	    lambda		return                                          #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
